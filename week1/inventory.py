2fd37b0b649d248724cc0d5e41306c4293bc63eb
Danielitux12 <danielillo3609@gmail.com>
2026-03-26 16:15:38 -0500

    code documentation

--- week1/inventory.py
+++ week1/inventory.py
@@ -1,43 +1,71 @@
+#Show the welcome message to the user#
 print("Welcome to inventory")
+# Function to validate the product name#
 def valid_product():
+    # Use a global variable to store the product name#
     global product
+     # Ask the user to enter the product name#
     product = str(input("Enter name of the product: "))
+    # Check if the product name contains only letters#
     if product.isalpha():
         print("PRODUCT VALID :>")
+        # Return the valid product name#
         return product
     else:
         print("Write a only name :)")
+        # Call the function again if invalid#
         valid_product()
+# Function to validate the product price#
 def valid_price():
     try:
+        # Use a global variable to store the price#
         global price
+        # Ask the user to enter the price#
         price = int (input("Enter price of the product: "))
+        # Check if the price is greater than 0#
         if price > 0:
             print("PRICE VALID :)")
         else:
             print("This price is negative :( ")
+             # Call the function again if invalid#
             valid_price()
+    # Handle the case where the input is not a number#
     except ValueError:
         print("Error, invalid price, try again :(")
+        # Call the function again if invalid#
         valid_price()
+# Function to validate the product quantity#
 def valid_quantity():
     try:
+        # Use a global variable to store the quantity#
         global quantity
+        # Ask the user to enter the quantity#
         quantity = int (input("Enter quantity of the product: "))
+        # Check if the quantity is greater than 0#
         if quantity > 0:
             print("QUANTITY VALID :D")
         else:
             print("This quantity is invalid :(")
+            # Call the function again if invalid#
             valid_quantity()
+    # Handle the case where the input is not a number#
     except ValueError:
         print("Error, invalid quantity, try again :)")
+        # Call the function again if invalid#
         valid_quantity()
+# Call the functions to get valid data from the user#
 valid_product()
 valid_price()
 valid_quantity()
+# Calculate the total cost (price multiplied by quantity)#
 total_cost = price * quantity
+# Print the total cost of the price and quantity#
 print("---INVENTORY---")
+# Show the product name#
 print(f"Product: {product}")
+# Show the price#
 print(f"Price: {price}")
+# Show the quantity#
 print(f"Quantity: {quantity}")
+# Show total cost#
 print(f"Total cost: {price} x {quantity} = {total_cost}")
